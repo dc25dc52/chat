5025fe21d18ca4e85ba98954172b5d7300b3c7b5
dc25dc52 <85923022+dc25dc52@users.noreply.github.com>
2022-03-10 16:34:45 +0800

    1

---
+++ app/src/main/java/com/tom/chat/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.tom.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.tom.chat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupFuctions()
+    }
+    fun setupFuctions(){
+        binding.bPerson.setOnClickListener {
+            //思考帶東西回來之類
+        }
+        //三顆按鈕的事情
+    }
+}
